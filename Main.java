8a3c32af667526a34d4addc7745f948918a57459
plamenufl <pulsar88@gmail.com>
2019-11-10 21:56:16 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,6 +11,7 @@ public class Main {
             System.out.println("Invalid Value");
         } else {
             int rowCount = 0;
+            // r stands for row and c stands for column
             for (int r = 1; r <= number; r++) {
 
                 for(int c = 1; c <= number; c++){
@@ -44,11 +45,4 @@ public class Main {
         }
     }
 }
-// if row = 2 and column = 2 rowcount is 1
-// subtract rowcount 1 from column(number)5  = column = 4 row = 2  rowcount++
 
-// if row = 3 and column = 3 rowcount = 2
-// subtract rowcount 2 from column(number)5  = column = 3 row = 2  rowcount++
-
-// if row = 4 and column = 4 rowcount = 3
-// subtract rowcount 3 from column (number)5 = column = 2 and row = 4
